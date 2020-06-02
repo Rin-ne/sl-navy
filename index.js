7a5013f2d762f1f5ce562769a2938e628c64c218
illyasviell <ashja234@gmail.com>
2020-06-02 17:20:43 +0700

    .

--- index.js
+++ index.js
@@ -1,72 +1,134 @@
 const express = require("express")
-const server  = express()
-const http    = require("http").createServer(server)
-const PORT    = process.env.PORT || 3000
-const health  = require("express-ping")
-const io      = require("socket.io")(http)
-const bp      = require("body-parser")
-const fs      = require("fs")
+const mongodb = require("mongodb")
+const server = express()
+const http = require("http").createServer(server)
+const PORT = process.env.PORT || 3000
+const health = require("express-ping")
+const io = require("socket.io")(http)
+const bp = require("body-parser")
+const fs = require("fs")
 
-server.use( health.ping() )
-server.use( bp({ limit: "10mb" }) )
-server.use( bp.urlencoded() )
-server.use( bp.urlencoded({ extended: true })) 
+const MongoClient = mongodb.MongoClient
+const uri = "mongodb+srv://SlimeDev:a1s2h3j4a5@cluster0-qnvfk.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+let db,
+  collection
+client.connect(err => {
+  if(err) throw err
+  console.log("connected to mongo database")
+  db = client.db("stackedChat")
+  collection = db.collection("slime")
 
-let numberOfConnectedClient = 0
-let disconnectedClient = 0
 
-server.get(
-  "/",
-  (request, response) => {
-    response.send(numberOfConnectedClient.toString())
-  }
-)
+  server.use(health.ping())
+  server.use(bp({ limit: "10mb" }))
+  server.use(bp.urlencoded())
+  server.use(bp.urlencoded({ extended: true }))
 
-server.get(
-  "/test",
-  (request, response) => {
-    response.sendFile(__dirname + "/view/test.html")
-  }
-)
+  let numberOfConnectedClient = 0
 
-io.on(
-  "connection",
-  (socket) => {
-    numberOfConnectedClient++
-    console.log(numberOfConnectedClient)
-    socket.on(
-      "disconnect",
-      () => {
-        numberOfConnectedClient--
+  server.get(
+    "/",
+    (request, response) => {
+      response.send(numberOfConnectedClient.toString())
+    }
+  )
+
+  server.get(
+    "/up",
+    (request, response) => {
+
+    }
+  )
+
+  server.get(
+    "/chats",
+    (request, response) => {
+      if (typeof request.query.nomor == "string") {
+        collection.find({ receiver: request.query.nomor }).toArray(
+          (err, $) => {
+            if (err) console.log(err)
+            console.log($)
+            response.send(JSON.stringify($))
+            collection.deleteMany({receiver: request.query.nomor})
+          }
+        )
       }
-    )
-    socket.on(
-      "video",
-      ($) => {
-        if (
-          typeof $.receiver == "string"
-          &&
-          typeof $.data     == "string"
-        ) {
+      else {
+        response.send("Err : Unknown query")
+      }
+    }
+  )
+
+  io.on(
+    "connection",
+    (socket) => {
+      numberOfConnectedClient++
+      socket.on(
+        "disconnect",
+        () => {
+          numberOfConnectedClient--
+        }
+      )
+      socket.on(
+        "video",
+        ($) => {
+          if (
+            typeof $.receiver == "string"
+            &&
+            typeof $.data == "string"
+          ) {
+            socket.broadcast.emit(
+              $.receiver,
+              JSON.stringify(
+                {
+                  data: $.data,
+                  date: $.date,
+                  time: $.time,
+                  sender: $.sender
+                }
+              )
+            )
+          }
+        }
+      )
+      socket.on(
+        "image",
+        ($) => {
           socket.broadcast.emit(
             $.receiver,
             JSON.stringify(
               {
-                data : $.data,
-                date : $.date,
-                time : $.time,
+                data: $.data,
+                date: $.date,
+                time: $.time,
+                sender: $.sender
               }
             )
           )
         }
-      }
-    )
-  }
-)
+      )
+      socket.on(
+        "store this chat please",
+        ($, onError) => {
+          if ($.type == "array") {
+            collection.insertMany($.data)
+          }
+          else if ($.type == "object") {
+            collection.insertOne($.data)
+          }
+          else {
+            onError("Error : Unknown Data type")
+          }
+        }
+      )
+    }
+  )
 
-http.listen(
-  PORT,
-  () => {
-    console.log("server started on port " + PORT)
-  }
-)
+  http.listen(
+    PORT,
+    () => {
+      console.log("server started on port " + PORT)
+    }
+  )
+});
